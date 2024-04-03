ee4dc1dd7ed5035e7a4e51b2fb8b8755423e9f82
Anton Korpusenko <antokorp@gmail.com>
2024-04-03 14:32:04 +0300

    added some benchmarks

---
+++ operator/keys/rsa_linux_bench_test.go
@@ -0,0 +1,65 @@
+//go:build linux
+
+package keys
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"github.com/microsoft/go-crypto-openssl/openssl"
+	"github.com/stretchr/testify/require"
+	"sync"
+	"testing"
+)
+
+func BenchmarkPrivKeyCache(b *testing.B) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(b, err)
+
+	pk := &privateKey{key, nil, sync.Once{}}
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			key, err := checkCachePrivkey(pk)
+			require.NoError(b, err)
+			require.NotNil(b, key)
+		}
+	})
+}
+
+type privateKeyWithRWMutex struct {
+	privKey       *rsa.PrivateKey
+	cachedPrivKey *openssl.PrivateKeyRSA
+	mu            sync.RWMutex
+}
+
+func (priv *privateKeyWithRWMutex) checkCachePrivkey() (*openssl.PrivateKeyRSA, error) {
+	priv.mu.RLock()
+	defer priv.mu.RUnlock()
+	if priv.cachedPrivKey != nil {
+		return priv.cachedPrivKey, nil
+	}
+	opriv, err := rsaPrivateKeyToOpenSSL(priv.privKey)
+	if err != nil {
+		return nil, err
+	}
+	priv.mu.Lock()
+	priv.cachedPrivKey = opriv
+	priv.mu.Unlock()
+
+	return opriv, nil
+}
+
+func BenchmarkPrivKeyCacheWithRWMutex(b *testing.B) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(b, err)
+
+	pk := &privateKeyWithRWMutex{key, nil, sync.RWMutex{}}
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			key, err := pk.checkCachePrivkey()
+			require.NoError(b, err)
+			require.NotNil(b, key)
+		}
+	})
+}
